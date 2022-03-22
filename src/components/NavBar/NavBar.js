d850a1a9ffe5a9628ae5d564b2242037475ca62f
dino perucca <ezequielperucca@gmail.com>
2022-03-21 23:06:40 -0300

    clase 2

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,14 @@
+import './NavBar.css'
+
+const NavBar = () => {
+    return(
+        <nav className="NavBar">
+            Ceramica 25 de Mayo
+            <button>ceramicas</button>
+            <button>materiales</button>
+            <button>envios</button>
+        </nav>
+    )
+}
+
+export default NavBar
